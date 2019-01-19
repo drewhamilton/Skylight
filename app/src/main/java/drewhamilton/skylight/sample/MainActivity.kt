7eeba94ba7533b5ebc42f59cc326aa4a96537e26
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-01-19 17:16:38 +0100

    Start adding layout for MainActivity

--- app/src/main/java/drewhamilton/skylight/sample/MainActivity.kt
+++ app/src/main/java/drewhamilton/skylight/sample/MainActivity.kt
@@ -1,6 +1,20 @@
 package drewhamilton.skylight.sample
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import drewhamilton.skylight.R
+import drewhamilton.skylight.sample.location.LocationRepository
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationRepository: LocationRepository
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+
+        // TODO: Use Dagger
+        locationRepository = LocationRepository(this)
+    }
 }
