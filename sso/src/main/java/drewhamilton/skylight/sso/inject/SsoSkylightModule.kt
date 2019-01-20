842e5c678e166bcc99d25695f4794dae8047e1d6
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-01-20 14:06:40 +0100

    Swap naming of 2 modules

--- sso/src/main/java/drewhamilton/skylight/sso/inject/SsoSkylightModule.kt
+++ sso/src/main/java/drewhamilton/skylight/sso/inject/SsoSkylightModule.kt
@@ -1,60 +1,19 @@
 package drewhamilton.skylight.sso.inject
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
+import dagger.Reusable
 import drewhamilton.skylight.SkylightRepository
-import drewhamilton.skylight.sso.BASE_URL_SUNRISE_SUNSET_ORG
-import drewhamilton.skylight.sso.SsoSkylightRepository
-import drewhamilton.skylight.sso.network.InfoClient
-import drewhamilton.skylight.sso.network.SsoApi
-import drewhamilton.skylight.sso.serialization.SsoDateTimeAdapter
-import io.reactivex.schedulers.Schedulers
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
-import javax.inject.Singleton
 
-@Module
-class SsoSkylightModule(private val httpLoggingInterceptor: HttpLoggingInterceptor? = null) {
+/**
+ * Use this module if you have no other implementations of [SkylightRepository] in your scope. If you are using multiple
+ * instances of [SkylightRepository] in the same scope, you can use [QualifiedSsoSkylightModule] along with the [Sso]
+ * qualifier instead.
+ */
+@Module(includes = [QualifiedSsoSkylightModule::class])
+class SsoSkylightModule {
 
     @Provides
-    @Sso
-    fun skylightRepository(client: InfoClient): SkylightRepository = SsoSkylightRepository(client)
-
-    @Provides
-    @Singleton
-    fun ssoApi(@Sso ssoRetrofit: Retrofit): SsoApi = ssoRetrofit.create(SsoApi::class.java)
-
-    @Provides
-    @Singleton
-    @Sso
-    internal fun retrofit(@Sso moshi: Moshi, @Sso okHttpClient: OkHttpClient) = Retrofit.Builder()
-        .baseUrl(BASE_URL_SUNRISE_SUNSET_ORG)
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .client(okHttpClient)
-        .build()
-
-    @Provides
-    @Sso
-    internal fun moshi(dateTimeAdapter: SsoDateTimeAdapter) = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .add(dateTimeAdapter)
-        .build()
-
-    @Provides
-    @Sso
-    internal fun okHttpClient(): OkHttpClient {
-        val builder = OkHttpClient().newBuilder()
-
-        httpLoggingInterceptor?.let {
-            builder.addInterceptor(httpLoggingInterceptor)
-        }
-
-        return builder.build()
-    }
+    @Reusable
+    fun skylightRepository(@Sso skylightRepository: SkylightRepository) = skylightRepository
 }
