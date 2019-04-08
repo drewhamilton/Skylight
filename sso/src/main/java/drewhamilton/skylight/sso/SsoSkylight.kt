68ed72ea64a5acb172731e21395c647779b1a237
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-04-08 17:32:33 +0200

    Fix SSO compilation error

--- sso/src/main/java/drewhamilton/skylight/sso/SsoSkylight.kt
+++ sso/src/main/java/drewhamilton/skylight/sso/SsoSkylight.kt
@@ -28,9 +28,9 @@ class SsoSkylight @Inject constructor(private val client: InfoClient) : Skylight
 }
 
 internal fun SunriseSunsetInfo.toSkylightInfo(): SkylightInfo = when {
-    civil_twilight_begin == ApiConstants.DATE_TIME_NONE && sunrise == ApiConstants.DATE_TIME_NONE -> NeverLight()
+    civil_twilight_begin == ApiConstants.DATE_TIME_NONE && sunrise == ApiConstants.DATE_TIME_NONE -> NeverLight
     civil_twilight_begin == ApiConstants.DATE_TIME_ALWAYS_DAY && sunrise == ApiConstants.DATE_TIME_ALWAYS_DAY ->
-        AlwaysDaytime()
+        AlwaysDaytime
     civil_twilight_begin == ApiConstants.DATE_TIME_NONE -> AlwaysLight(sunrise, sunset)
     sunrise == ApiConstants.DATE_TIME_NONE -> NeverDaytime(civil_twilight_begin, civil_twilight_end)
     else -> Typical(civil_twilight_begin, sunrise, sunset, civil_twilight_end)
