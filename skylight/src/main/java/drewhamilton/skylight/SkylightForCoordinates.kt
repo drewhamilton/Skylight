5864bb1e6e8f767cd810ac1d05fb00ccd143700d
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-04-12 17:12:20 +0200

    Implement docs and tests for SkylightForCoordinates

--- skylight/src/main/java/drewhamilton/skylight/SkylightForCoordinates.kt
+++ skylight/src/main/java/drewhamilton/skylight/SkylightForCoordinates.kt
@@ -10,9 +10,23 @@ class SkylightForCoordinates @Inject constructor(
     internal val skylight: Skylight,
     val coordinates: Coordinates
 ) {
+    /**
+     * @param date The date for which to return info. The time information in this parameter is ignored.
+     * @return [SkylightDay] at this object's coordinates for the given date.
+     */
     fun getSkylightDay(date: Date) = skylight.getSkylightDay(coordinates, date)
 }
 
+/**
+ * @param dateTime The date-time at which to check for lightness.
+ * @return Whether it is light outside at the [SkylightForCoordinates]'s coordinates at the given date-time, where
+ * "light" means after dawn and before dusk on the given date.
+ */
 fun SkylightForCoordinates.isLight(dateTime: Date) = skylight.isLight(coordinates, dateTime)
 
+/**
+ * @param dateTime The date-time at which to check for darkness.
+ * @return Whether it is dark outside at the [SkylightForCoordinates]'s coordinates at the given date-time, where "dark"
+ * means before dawn or after dusk on the given date.
+ */
 fun SkylightForCoordinates.isDark(dateTime: Date) = skylight.isDark(coordinates, dateTime)
---
+++ skylight/src/test/java/drewhamilton/skylight/SkylightForCoordinatesTest.kt
@@ -0,0 +1,74 @@
+package drewhamilton.skylight
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import java.util.Date
+import kotlin.reflect.KClass
+
+class SkylightForCoordinatesTest {
+
+    private lateinit var mockSkylight: Skylight
+    private val testCoordinates = Coordinates(98.7, 6.54)
+
+    private lateinit var skylightForCoordinates: SkylightForCoordinates
+
+    @Before
+    fun setUp() {
+        mockSkylight = mock()
+        skylightForCoordinates = SkylightForCoordinates(mockSkylight, testCoordinates)
+    }
+
+    @Test
+    fun `getSkylightDay passes coordinates and date to Skylight`() {
+        val testDate = Date(54321)
+        skylightForCoordinates.getSkylightDay(testDate)
+
+        verify(mockSkylight).getSkylightDay(testCoordinates, testDate)
+        verifyNoMoreInteractions(mockSkylight)
+    }
+
+    @Test
+    fun `isLight forwards to Skylight isLight`() {
+        skylightForCoordinates = SkylightForCoordinates(mockSkylight, testCoordinates)
+
+        val testInputDate = Date(29385723847)
+        val testCoordinates = Coordinates(1.23, 45.6)
+        for (kClass in SkylightDay::class.sealedSubclasses) {
+            whenever(mockSkylight.getSkylightDay(any(), any())).thenReturn(kClass.instantiate(Date(234234)))
+            assertEquals(
+                mockSkylight.isLight(testCoordinates, testInputDate),
+                skylightForCoordinates.isLight(testInputDate)
+            )
+        }
+    }
+
+    @Test
+    fun `isDark forwards to Skylight isDark`() {
+        skylightForCoordinates = SkylightForCoordinates(mockSkylight, testCoordinates)
+
+        val testInputDate = Date(239487)
+        val testCoordinates = Coordinates(12.3, 4.56)
+        for (kClass in SkylightDay::class.sealedSubclasses) {
+            whenever(mockSkylight.getSkylightDay(any(), any())).thenReturn(kClass.instantiate(Date(9823743)))
+            assertEquals(
+                mockSkylight.isDark(testCoordinates, testInputDate),
+                skylightForCoordinates.isDark(testInputDate)
+            )
+        }
+    }
+
+    private fun KClass<out SkylightDay>.instantiate(date: Date) = when(this) {
+        SkylightDay.Typical::class -> SkylightDay.Typical(date, date, date, date)
+        SkylightDay.AlwaysDaytime::class -> SkylightDay.AlwaysDaytime
+        SkylightDay.AlwaysLight::class -> SkylightDay.AlwaysLight(date, date)
+        SkylightDay.NeverDaytime::class -> SkylightDay.NeverDaytime(date, date)
+        SkylightDay.NeverLight::class -> SkylightDay.NeverLight
+        else -> throw IllegalArgumentException("Unknown ${SkylightDay::class} subtype: $this")
+    }
+}
