b3ae3ce76d125b54f04fe7a3e8e9212ec2db3fb2
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-05-11 11:33:04 +0200

    Add settings option to app bar menu

--- app/src/main/java/drewhamilton/skylight/sample/main/LocationSpinnerAdapter.kt
+++ app/src/main/java/drewhamilton/skylight/sample/main/LocationSpinnerAdapter.kt
@@ -6,5 +6,5 @@ import android.widget.ArrayAdapter
 import drewhamilton.skylight.sample.R
 import drewhamilton.skylight.sample.location.Location
 
-class LocationSpinnerAdapter(context: Context, locations: List<Location>) :
-    ArrayAdapter<Location>(context, R.layout.item_spinner, locations)
+class LocationSpinnerAdapter(context: Context, locations: List<Location>)
+    : ArrayAdapter<Location>(context, R.layout.item_spinner, locations)
