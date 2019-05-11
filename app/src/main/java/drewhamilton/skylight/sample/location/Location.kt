2c7fa88cd49318da6adb8dea01a8b859aa42e217
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-05-11 12:16:32 +0200

    Fix Honolulu coordinates

--- app/src/main/java/drewhamilton/skylight/sample/location/Location.kt
+++ app/src/main/java/drewhamilton/skylight/sample/location/Location.kt
@@ -23,7 +23,7 @@ enum class ExampleLocation(
 ) {
     AMSTERDAM(R.string.amsterdam, TimeZone.getTimeZone("Europe/Amsterdam"), Coordinates(52.3680, 4.9036)),
     BANGKOK(R.string.bangkok, TimeZone.getTimeZone("Asia/Bangkok"), Coordinates(13.7563, 100.5018)),
-    HONOLULU(R.string.honolulu, TimeZone.getTimeZone("Pacific/Honolulu"), Coordinates(21.3069, 157.8583)),
+    HONOLULU(R.string.honolulu, TimeZone.getTimeZone("Pacific/Honolulu"), Coordinates(21.3069, -157.8583)),
     ISTANBUL(R.string.istanbul, TimeZone.getTimeZone("Europe/Istanbul"), Coordinates(41.0082, 28.9784)),
     LIMA(R.string.lima, TimeZone.getTimeZone("America/Lima"), Coordinates(-12.0464, -77.0428)),
     MARRAKESH(R.string.marrakesh, TimeZone.getTimeZone("Africa/Casablanca"), Coordinates(31.6295, -7.9811)),
