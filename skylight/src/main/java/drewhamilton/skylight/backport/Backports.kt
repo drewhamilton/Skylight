e4a408ae2fd658463ccc2be6fd016e9ec71bbd6d
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-05-21 18:38:02 +0200

    Remove ThreeTenBP from :skylight

--- skylight/src/main/java/drewhamilton/skylight/backport/Backports.kt
+++
@@ -1,7 +0,0 @@
-package drewhamilton.skylight.backport
-
-import java.time.LocalDate
-
-//internal fun LocalDate.backport(): org.threeten.bp.LocalDate {
-//    return org.threeten.bp.LocalDate.of(year, month.value, dayOfMonth)
-//}
