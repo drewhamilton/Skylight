1de147a514c5cebc3952be1dbeb74e1999e4f15d
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-01-13 18:12:55 +0100

    Fix :rx test compilation with new initializer

--- rx/src/test/java/drewhamilton/skylight/rx/TestHelpers.kt
+++ rx/src/test/java/drewhamilton/skylight/rx/TestHelpers.kt
@@ -7,11 +7,12 @@ import java.time.ZonedDateTime
 
 private const val timeDifferenceSeconds = 5L
 
-internal fun dummyTypical(date: LocalDate, dawn: OffsetTime) = SkylightDay.Typical(
-    zonedDateTimeOf(date, dawn),
-    zonedDateTimeOf(date, dawn.plusSeconds(timeDifferenceSeconds)),
-    zonedDateTimeOf(date, dawn.plusSeconds(2 * timeDifferenceSeconds)),
-    zonedDateTimeOf(date, dawn.plusSeconds(3 * timeDifferenceSeconds))
-)
+internal fun dummyTypical(date: LocalDate, dawn: OffsetTime) = SkylightDay.Typical {
+    this.date = date
+    this.dawn = zonedDateTimeOf(date, dawn)
+    this.sunrise = zonedDateTimeOf(date, dawn.plusSeconds(timeDifferenceSeconds))
+    this.sunset = zonedDateTimeOf(date, dawn.plusSeconds(2 * timeDifferenceSeconds))
+    this.dusk = zonedDateTimeOf(date, dawn.plusSeconds(3 * timeDifferenceSeconds))
+}
 
 private fun zonedDateTimeOf(date: LocalDate, time: OffsetTime) = ZonedDateTime.of(date, time.toLocalTime(), time.offset)
