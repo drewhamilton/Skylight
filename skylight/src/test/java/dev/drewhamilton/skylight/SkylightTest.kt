2c1db417f37eabd14ca04d56947b37196ef4de08
Drew Hamilton <github@drewhamilton.dev>
2022-11-09 18:32:43 -0800

    Remove mocking in tests (#40)
    * Remove Mockito from :skylight module
    * Remove Mockito from :sunrise-sunset-org module

--- skylight/src/test/java/dev/drewhamilton/skylight/SkylightTest.kt
+++ skylight/src/test/java/dev/drewhamilton/skylight/SkylightTest.kt
@@ -1,5 +1,6 @@
 package dev.drewhamilton.skylight
 
+import dev.drewhamilton.skylight.test.TestSkylight
 import java.time.LocalDate
 import java.time.Month
 import java.time.ZoneOffset
@@ -11,222 +12,184 @@ import org.junit.Test
 class SkylightTest {
 
     private val testCoordinates = Coordinates(50.0, 60.0)
-    private val testDate = LocalDate.of(2019, Month.MAY, 15)
+    private val typicalDate = LocalDate.of(2019, Month.MAY, 15)
+    private val alwaysDaytimeDate = LocalDate.of(2019, Month.JUNE, 15)
+    private val alwaysLightDate = LocalDate.of(2019, Month.JULY, 15)
+    private val neverLightDate = LocalDate.of(2019, Month.AUGUST, 15)
+    private val neverDaytimeDate = LocalDate.of(2019, Month.SEPTEMBER, 15)
+
     private val testDawn = ZonedDateTime.of(2019, 5, 15, 8, 0, 0, 0, ZoneOffset.UTC)
     private val testSunrise = ZonedDateTime.of(2019, 5, 15, 10, 0, 0, 0, ZoneOffset.UTC)
     private val testSunset = ZonedDateTime.of(2019, 5, 15, 14, 0, 0, 0, ZoneOffset.UTC)
     private val testDusk = ZonedDateTime.of(2019, 5, 15, 16, 0, 0, 0, ZoneOffset.UTC)
 
-    private val beforeDawn = ZonedDateTime.of(testDate, testDawn.minusHours(2).toLocalTime(), ZoneOffset.UTC)
+    private val beforeDawn = ZonedDateTime.of(typicalDate, testDawn.minusHours(2).toLocalTime(), ZoneOffset.UTC)
     private val betweenSunriseAndSunset =
-        ZonedDateTime.of(testDate, testSunrise.plusHours(2).toLocalTime(), ZoneOffset.UTC)
-    private val afterDusk = ZonedDateTime.of(testDate, testDusk.plusHours(2).toLocalTime(), ZoneOffset.UTC)
-    private val beforeSunrise = ZonedDateTime.of(testDate, testSunrise.minusHours(1).toLocalTime(), ZoneOffset.UTC)
-    private val afterSunset = ZonedDateTime.of(testDate, testSunset.plusHours(1).toLocalTime(), ZoneOffset.UTC)
+        ZonedDateTime.of(typicalDate, testSunrise.plusHours(2).toLocalTime(), ZoneOffset.UTC)
+    private val afterDusk = ZonedDateTime.of(typicalDate, testDusk.plusHours(2).toLocalTime(), ZoneOffset.UTC)
+    private val beforeSunrise = ZonedDateTime.of(typicalDate, testSunrise.minusHours(1).toLocalTime(), ZoneOffset.UTC)
+    private val afterSunset = ZonedDateTime.of(typicalDate, testSunset.plusHours(1).toLocalTime(), ZoneOffset.UTC)
 
-    private val testAlwaysDaytime = SkylightDay.AlwaysDaytime(date = testDate)
+    private val testAlwaysDaytime = SkylightDay.AlwaysDaytime(date = alwaysDaytimeDate)
     private val testAlwaysLight = SkylightDay.Typical(
-        date = testDate,
-        sunrise = testSunrise.toInstant(),
-        sunset = testSunset.toInstant()
+        date = alwaysLightDate,
+        sunrise = testSunrise.withDate(alwaysLightDate).toInstant(),
+        sunset = testSunset.withDate(alwaysLightDate).toInstant()
     )
-    private val testNeverLight = SkylightDay.NeverLight(date = testDate)
+    private val testNeverLight = SkylightDay.NeverLight(date = neverLightDate)
     private val testNeverDaytime = SkylightDay.Typical(
-        date = testDate,
-        dawn = testDawn.toInstant(),
-        dusk = testDusk.toInstant()
+        date = neverDaytimeDate,
+        dawn = testDawn.withDate(neverDaytimeDate).toInstant(),
+        dusk = testDusk.withDate(neverDaytimeDate).toInstant()
     )
     private val testTypical = SkylightDay.Typical (
-        date = testDate,
+        date = typicalDate,
         dawn = testDawn.toInstant(),
         sunrise = testSunrise.toInstant(),
         sunset = testSunset.toInstant(),
         dusk = testDusk.toInstant()
     )
 
+    private val skylight = TestSkylight(
+        testCoordinates to alwaysDaytimeDate to testAlwaysDaytime,
+        testCoordinates to alwaysLightDate to testAlwaysLight,
+        testCoordinates to neverLightDate to testNeverLight,
+        testCoordinates to neverDaytimeDate to testNeverDaytime,
+        testCoordinates to typicalDate to testTypical,
+    )
+
     //region isLight
     @Test fun `isLight with AlwaysDaytime returns true`() {
-        val skylight = FakeSkylight(testAlwaysDaytime)
-
-        assertTrue(skylight.isLight(testCoordinates, beforeDawn))
-        assertTrue(skylight.isLight(testCoordinates, betweenSunriseAndSunset))
-        assertTrue(skylight.isLight(testCoordinates, afterDusk))
+        assertTrue(skylight.isLight(testCoordinates, beforeDawn.withDate(alwaysDaytimeDate)))
+        assertTrue(skylight.isLight(testCoordinates, betweenSunriseAndSunset.withDate(alwaysDaytimeDate)))
+        assertTrue(skylight.isLight(testCoordinates, afterDusk.withDate(alwaysDaytimeDate)))
     }
 
     @Test fun `isLight with AlwaysLight returns true`() {
-        val skylight = FakeSkylight(testAlwaysLight)
-
-        assertTrue(skylight.isLight(testCoordinates, beforeDawn))
-        assertTrue(skylight.isLight(testCoordinates, betweenSunriseAndSunset))
-        assertTrue(skylight.isLight(testCoordinates, afterDusk))
+        assertTrue(skylight.isLight(testCoordinates, beforeDawn.withDate(alwaysLightDate)))
+        assertTrue(skylight.isLight(testCoordinates, betweenSunriseAndSunset.withDate(alwaysLightDate)))
+        assertTrue(skylight.isLight(testCoordinates, afterDusk.withDate(alwaysLightDate)))
     }
 
     @Test fun `isLight with NeverLight returns false`() {
-        val skylight = FakeSkylight(testNeverLight)
-
-        assertFalse(skylight.isLight(testCoordinates, beforeDawn))
-        assertFalse(skylight.isLight(testCoordinates, betweenSunriseAndSunset))
-        assertFalse(skylight.isLight(testCoordinates, afterDusk))
+        assertFalse(skylight.isLight(testCoordinates, beforeDawn.withDate(neverLightDate)))
+        assertFalse(skylight.isLight(testCoordinates, betweenSunriseAndSunset.withDate(neverLightDate)))
+        assertFalse(skylight.isLight(testCoordinates, afterDusk.withDate(neverLightDate)))
     }
 
     @Test fun `isLight with NeverDaytime returns false before dawn`() {
-        val skylight = FakeSkylight(testNeverDaytime)
-
-        assertFalse(skylight.isLight(testCoordinates, beforeDawn))
+        assertFalse(skylight.isLight(testCoordinates, beforeDawn.withDate(neverDaytimeDate)))
     }
 
     @Test fun `isLight with NeverDaytime returns true between dawn and dusk`() {
-        val skylight = FakeSkylight(testNeverDaytime)
-
-        assertTrue(skylight.isLight(testCoordinates, betweenSunriseAndSunset))
+        assertTrue(skylight.isLight(testCoordinates, betweenSunriseAndSunset.withDate(neverDaytimeDate)))
     }
 
     @Test fun `isLight with NeverDaytime returns false after dusk`() {
-        val skylight = FakeSkylight(testNeverDaytime)
-
-        assertFalse(skylight.isLight(testCoordinates, afterDusk))
+        assertFalse(skylight.isLight(testCoordinates, afterDusk.withDate(neverDaytimeDate)))
     }
 
     @Test fun `isLight with Typical returns false before dawn`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertFalse(skylight.isLight(testCoordinates, beforeDawn))
+        assertFalse(skylight.isLight(testCoordinates, beforeDawn.withDate(typicalDate)))
     }
 
     @Test fun `isLight with Typical returns true between dawn and dusk`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertTrue(skylight.isLight(testCoordinates, betweenSunriseAndSunset))
+        assertTrue(skylight.isLight(testCoordinates, betweenSunriseAndSunset.withDate(typicalDate)))
     }
 
     @Test fun `isLight with Typical returns false after dusk`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertFalse(skylight.isLight(testCoordinates, afterDusk))
+        assertFalse(skylight.isLight(testCoordinates, afterDusk.withDate(typicalDate)))
     }
     //endregion
 
     //region isDark
     @Test fun `isDark with AlwaysDaytime returns false`() {
-        val skylight = FakeSkylight(testAlwaysDaytime)
-
-        assertFalse(skylight.isDark(testCoordinates, beforeDawn))
-        assertFalse(skylight.isDark(testCoordinates, betweenSunriseAndSunset))
-        assertFalse(skylight.isDark(testCoordinates, afterDusk))
+        assertFalse(skylight.isDark(testCoordinates, beforeDawn.withDate(alwaysDaytimeDate)))
+        assertFalse(skylight.isDark(testCoordinates, betweenSunriseAndSunset.withDate(alwaysDaytimeDate)))
+        assertFalse(skylight.isDark(testCoordinates, afterDusk.withDate(alwaysDaytimeDate)))
     }
 
     @Test fun `isDark with AlwaysLight returns false`() {
-        val skylight = FakeSkylight(testAlwaysLight)
-
-        assertFalse(skylight.isDark(testCoordinates, beforeDawn))
-        assertFalse(skylight.isDark(testCoordinates, betweenSunriseAndSunset))
-        assertFalse(skylight.isDark(testCoordinates, afterDusk))
+        assertFalse(skylight.isDark(testCoordinates, beforeDawn.withDate(alwaysLightDate)))
+        assertFalse(skylight.isDark(testCoordinates, betweenSunriseAndSunset.withDate(alwaysLightDate)))
+        assertFalse(skylight.isDark(testCoordinates, afterDusk.withDate(alwaysLightDate)))
     }
 
     @Test fun `isDark with NeverLight returns true`() {
-        val skylight = FakeSkylight(testNeverLight)
-
-        assertTrue(skylight.isDark(testCoordinates, beforeDawn))
-        assertTrue(skylight.isDark(testCoordinates, betweenSunriseAndSunset))
-        assertTrue(skylight.isDark(testCoordinates, afterDusk))
+        assertTrue(skylight.isDark(testCoordinates, beforeDawn.withDate(neverLightDate)))
+        assertTrue(skylight.isDark(testCoordinates, betweenSunriseAndSunset.withDate(neverLightDate)))
+        assertTrue(skylight.isDark(testCoordinates, afterDusk.withDate(neverLightDate)))
     }
 
     @Test fun `isDark with NeverDaytime returns true before dawn`() {
-        val skylight = FakeSkylight(testNeverDaytime)
-
-        assertTrue(skylight.isDark(testCoordinates, beforeDawn))
+        assertTrue(skylight.isDark(testCoordinates, beforeDawn.withDate(neverDaytimeDate)))
     }
 
     @Test fun `isDark with NeverDaytime returns false between dawn and dusk`() {
-        val skylight = FakeSkylight(testNeverDaytime)
-
-        assertFalse(skylight.isDark(testCoordinates, betweenSunriseAndSunset))
+        assertFalse(skylight.isDark(testCoordinates, betweenSunriseAndSunset.withDate(neverDaytimeDate)))
     }
 
     @Test fun `isDark with NeverDaytime returns true after dusk`() {
-        val skylight = FakeSkylight(testNeverDaytime)
-
-        assertTrue(skylight.isDark(testCoordinates, afterDusk))
+        assertTrue(skylight.isDark(testCoordinates, afterDusk.withDate(neverDaytimeDate)))
     }
 
     @Test fun `isDark with Typical returns true before dawn`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertTrue(skylight.isDark(testCoordinates, beforeDawn))
+        assertTrue(skylight.isDark(testCoordinates, beforeDawn.withDate(typicalDate)))
     }
 
     @Test fun `isDark with Typical returns false between dawn and dusk`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertFalse(skylight.isDark(testCoordinates, betweenSunriseAndSunset))
+        assertFalse(skylight.isDark(testCoordinates, betweenSunriseAndSunset.withDate(typicalDate)))
     }
 
     @Test fun `isDark with Typical returns true after dusk`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertTrue(skylight.isDark(testCoordinates, afterDusk))
+        assertTrue(skylight.isDark(testCoordinates, afterDusk.withDate(typicalDate)))
     }
     //endregion
 
     //region isDaytime
     @Test fun `isDaytime with AlwaysDaytime returns true`() {
-        val skylight = FakeSkylight(testAlwaysDaytime)
-
-        assertTrue(skylight.isDaytime(testCoordinates, beforeDawn))
-        assertTrue(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset))
-        assertTrue(skylight.isDaytime(testCoordinates, afterDusk))
+        assertTrue(skylight.isDaytime(testCoordinates, beforeDawn.withDate(alwaysDaytimeDate)))
+        assertTrue(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset.withDate(alwaysDaytimeDate)))
+        assertTrue(skylight.isDaytime(testCoordinates, afterDusk.withDate(alwaysDaytimeDate)))
     }
 
     @Test fun `isDaytime with AlwaysLight returns false before sunrise`() {
-        val skylight = FakeSkylight(testAlwaysLight)
-
-        assertFalse(skylight.isDaytime(testCoordinates, beforeSunrise))
+        assertFalse(skylight.isDaytime(testCoordinates, beforeSunrise.withDate(alwaysLightDate)))
     }
 
     @Test fun `isDaytime with AlwaysLight returns true between sunrise and sunset`() {
-        val skylight = FakeSkylight(testAlwaysLight)
-
-        assertTrue(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset))
+        assertTrue(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset.withDate(alwaysLightDate)))
     }
 
     @Test fun `isDaytime with AlwaysLight returns false after sunset`() {
-        val skylight = FakeSkylight(testAlwaysLight)
-
-        assertFalse(skylight.isDaytime(testCoordinates, afterDusk))
+        assertFalse(skylight.isDaytime(testCoordinates, afterDusk.withDate(alwaysLightDate)))
     }
 
     @Test fun `isDaytime with NeverLight returns false`() {
-        val skylight = FakeSkylight(testNeverLight)
-
-        assertFalse(skylight.isDaytime(testCoordinates, beforeDawn))
-        assertFalse(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset))
-        assertFalse(skylight.isDaytime(testCoordinates, afterDusk))
+        assertFalse(skylight.isDaytime(testCoordinates, beforeDawn.withDate(neverLightDate)))
+        assertFalse(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset.withDate(neverLightDate)))
+        assertFalse(skylight.isDaytime(testCoordinates, afterDusk.withDate(neverLightDate)))
     }
 
     @Test fun `isDaytime with NeverDaytime returns false`() {
-        val skylight = FakeSkylight(testNeverDaytime)
-
-        assertFalse(skylight.isDaytime(testCoordinates, beforeSunrise))
-        assertFalse(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset))
-        assertFalse(skylight.isDaytime(testCoordinates, afterSunset))
+        assertFalse(skylight.isDaytime(testCoordinates, beforeSunrise.withDate(neverDaytimeDate)))
+        assertFalse(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset.withDate(neverDaytimeDate)))
+        assertFalse(skylight.isDaytime(testCoordinates, afterSunset.withDate(neverDaytimeDate)))
     }
 
     @Test fun `isDaytime with Typical returns false before sunrise`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertFalse(skylight.isDaytime(testCoordinates, beforeSunrise))
+        assertFalse(skylight.isDaytime(testCoordinates, beforeSunrise.withDate(typicalDate)))
     }
 
     @Test fun `isDaytime with Typical returns true between sunrise and sunset`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertTrue(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset))
+        assertTrue(skylight.isDaytime(testCoordinates, betweenSunriseAndSunset.withDate(typicalDate)))
     }
 
     @Test fun `isDaytime with Typical returns false after sunset`() {
-        val skylight = FakeSkylight(testTypical)
-
-        assertFalse(skylight.isDaytime(testCoordinates, afterSunset))
+        assertFalse(skylight.isDaytime(testCoordinates, afterSunset.withDate(typicalDate)))
     }
     //endregion
+
+    private fun ZonedDateTime.withDate(date: LocalDate): ZonedDateTime =
+        withYear(date.year).withMonth(date.monthValue).withDayOfMonth(date.dayOfMonth)
 }
